233bfc19da36118f7439c1c5d5375f3ae4dfca9a
Craig Wilson <craiggwilson@gmail.com>
2013-08-03 15:16:27 -0500

    added RemoveOperation tests.

---
+++ MongoDB.Driver.Core.FunctionalTests/Operations/RemoveOperation/When_removing_a_document_with_a_valid_query.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using NUnit.Framework;
+
+namespace MongoDB.Driver.Core.Operations.RemoveOperationSpecs
+{
+    public class When_removing_a_document_with_a_valid_query : Specification
+    {
+        protected override void Given()
+        {
+            InsertData(new BsonDocument("_id", 1).Add("x", 2));
+        }
+
+        protected override void When()
+        {
+            using (var session = BeginSession())
+            {
+                var op = new RemoveOperation
+                {
+                    Collection = _collection,
+                    Query = new BsonDocument("_id", 1),
+                    Session = session
+                };
+
+                op.Execute();
+            }
+        }
+
+        [Test]
+        public void The_document_should_be_removed()
+        {
+            var result = FindOne<BsonDocument>(new BsonDocument("_id", 1));
+            Assert.IsNull(result);
+        }
+    }
+}
---
+++ MongoDB.Driver.Core.FunctionalTests/Operations/RemoveOperation/When_removing_a_document_with_an_valid_query.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using NUnit.Framework;
+
+namespace MongoDB.Driver.Core.Operations.RemoveOperationSpecs
+{
+    public class When_removing_a_document_with_an_invalid_query : Specification
+    {
+        private Exception _exception;
+
+        protected override void Given()
+        {
+            InsertData(new BsonDocument("loc", new BsonArray(new double[] { 1, 2 })));
+        }
+
+        protected override void When()
+        {
+            using (var session = BeginSession())
+            {
+                var op = new RemoveOperation
+                {
+                    Collection = _collection,
+                    // this is an invalid query specification, $near requires an array 
+                    // as well as a 2d index
+                    Query = new BsonDocument("loc", new BsonDocument("$near", 2)),
+                    Session = session
+                };
+
+                _exception = Catch(() => op.Execute());
+            }
+        }
+
+
+        [Test]
+        public void An_exception_should_be_thrown()
+        {
+            Assert.IsNotNull(_exception);
+        }
+
+        [Test]
+        public void The_exception_should_be_a_MongoWriteConcernException()
+        {
+            Assert.IsInstanceOf<MongoWriteConcernException>(_exception);
+        }
+    }
+}
