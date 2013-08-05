5e1af3a80c88510f32b9eb9f9db0a0946f3d4950
Craig Wilson <craiggwilson@gmail.com>
2013-08-05 17:20:11 -0500

    adding more cursor tests.

---
+++ MongoDB.Driver.Core.FunctionalTests/Operations/CursorSpecs/When_the_limit_is_hit_on_the_initial_batch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver.Core.Protocol.Messages;
+using MongoDB.Driver.Core.Sessions;
+using NUnit.Framework;
+
+namespace MongoDB.Driver.Core.Operations
+{
+    public class When_the_limit_is_hit_on_the_initial_batch : Specification
+    {
+        private ICursor<BsonDocument> _cursor;
+
+        protected override void Given()
+        {
+            var docs = new List<BsonDocument>();
+            for (int i = 0; i < 10; i++)
+            {
+                docs.Add(new BsonDocument("_id", i));
+            }
+
+            InsertData(docs.ToArray());
+        }
+
+        protected override void When()
+        {
+            var session = BeginSession();
+            var findOp = new QueryOperation<BsonDocument>
+            {
+                Collection = _collection,
+                Limit = 2,
+                Query = new BsonDocument(),
+                Session = session
+            };
+
+            _cursor = findOp.Execute();
+            while (_cursor.MoveNext()) ;
+            Assert.AreEqual(0, _cursor.CurrentBatch);
+        }
+
+        [Test]
+        public void MoveNext_should_return_false_once_the_limit_has_been_reached()
+        {
+            Assert.IsFalse(_cursor.MoveNext());
+        }
+    }
+}
