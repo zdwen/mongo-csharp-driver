4a05f4d0e93fca1a7d0a8eb139439aa43eb6892d
Craig Wilson <craiggwilson@gmail.com>
2013-08-05 16:36:18 -0500

    added query tests for limit and batch size.

---
+++ MongoDB.Driver.Core.FunctionalTests/Operations/QueryOperationSpecs/When_querying_with_a_positive_limit_and_batch_size.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using NUnit.Framework;
+
+namespace MongoDB.Driver.Core.Operations.QueryOperationSpecs
+{
+    public class When_querying_with_a_positive_limit_and_batch_size : Specification
+    {
+        private List<BsonDocument> _results;
+
+        protected override void Given()
+        {
+            var docs = new List<BsonDocument>();
+            for (int i = 0; i < 10; i++)
+            {
+                docs.Add(new BsonDocument("_id", i));
+            }
+
+            InsertData(docs.ToArray());
+        }
+
+        protected override void When()
+        {
+            using (var session = BeginSession())
+            {
+                var findOp = new QueryOperation<BsonDocument>
+                {
+                    BatchSize = 2,
+                    Collection = _collection,
+                    Limit = 4,
+                    Query = new BsonDocument(),
+                    Session = session
+                };
+
+                _results = findOp.ToList();
+            }
+        }
+
+        [Test]
+        public void The_correct_number_of_documents_should_be_returned()
+        {
+            Assert.AreEqual(4, _results.Count);
+        }
+    }
+}
